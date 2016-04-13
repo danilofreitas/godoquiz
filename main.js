4d3092f5aa50329bd1dae80e1839061229695150
danilofreitas <danilogomesf@gmail.com>
2016-04-13 10:00:41 +0200

    now items are shown one time per iteration

--- main.js
+++ main.js
@@ -1,38 +1,56 @@
-(function($) {
+(function($, _) {
 
-  var randomExcuses = [
+  $(window).load(function () {
+    var randomExcuses = [
       "É problema de cache, limpa o cache do seu navegador...",
       "É problema da API!",
       "Isso só acontece no retina...",
       "Foi o Pepe que fez isso ai...",
-      "Is this a baby? No, it's a dog"
-  ];
+      "Is this a baby? No, it's a dog",
+      "Putz, no angular é diferente, é melhor mudar a API e o layout",
+      "Porra Pepe!!!",
+      "É melhor a API mandar tudo"
+    ];
 
-  function getRandomExcuse() {
-    return randomExcuses[Math.floor(Math.random() * randomExcuses.length)];
-  }
+    var excusesHolder = [];
 
-  $.fn.fadeTo = function(target) {
-    var e = $(this);
-    e.fadeOut(300, "swing", function() {
-      e.html(target);
-      e.fadeIn(200, "swing");
-    })
-  };
+    function getRandomExcuse() {
+      var currentExcuse = null;
+      randomExcuses = _.shuffle(randomExcuses);
+      if (randomExcuses.length > 0) {
+        currentExcuse = randomExcuses.shift();
+        excusesHolder.push(currentExcuse);
+        return currentExcuse;
+      }
 
-  $(document).keyup(function(k) {
-    if (k.keyCode == 32) {
-      $("h1").fadeTo(getRandomExcuse());
+      //Reset
+      randomExcuses = _.shuffle(excusesHolder);
+      excusesHolder = [];
     }
+
+    $.fn.fadeTo = function(target) {
+      var e = $(this);
+      e.fadeOut(300, "swing", function() {
+        e.html(target);
+        e.fadeIn(200, "swing");
+      })
+    };
+
+    $(document).keyup(function(k) {
+      if (k.keyCode == 32) {
+        $("h1").fadeTo(getRandomExcuse());
+      }
+    });
+
+    $(".tweet").click(function(a) {
+      a.preventDefault();
+
+      var b = encodeURIComponent('"' + $("h1").html() + '" - Gere a sua desculpa completamente aleatória no');
+      window.open("http://twitter.com/share?url=" + location.href + "&text=" + b + "&related=ferodss&", "twitterwindow", "height=450, width=550, top=" + ($(window).height() / 2 - 225) + ", left=" + $(window).width() / 2 + ", toolbar=0, location=0, menubar=0, directories=0, scrollbars=0")
+    });
+
+    $("h1").fadeTo(getRandomExcuse());
   });
 
-  $(".tweet").click(function(a) {
-    a.preventDefault();
 
-    var b = encodeURIComponent('"' + $("h1").html() + '" - Gere a sua desculpa completamente aleatória no');
-    window.open("http://twitter.com/share?url=" + location.href + "&text=" + b + "&related=ferodss&", "twitterwindow", "height=450, width=550, top=" + ($(window).height() / 2 - 225) + ", left=" + $(window).width() / 2 + ", toolbar=0, location=0, menubar=0, directories=0, scrollbars=0")
-  });
-
-  $("h1").fadeTo(getRandomExcuse());
-
-})(jQuery);
+})(jQuery, _);
